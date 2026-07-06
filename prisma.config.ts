5f0fd60a4459f37b04f15b8929f689fd75590fa7
William Galvão <willgalvao.developer@gmail.com>
2026-07-06 10:13:37 -0300

    chore(infra): add Express, Multer and migrate product schema with userId

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from "prisma/config";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
+  datasource: {
+    url: process.env["DATABASE_URL"]!, // <-- ADICIONE ISSO AQUI para alimentar o migrate CLI!
+  },
   migrations: {
     path: "prisma/migrations",
   },
